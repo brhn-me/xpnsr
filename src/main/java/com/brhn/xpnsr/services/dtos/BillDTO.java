cc2b48eeabbf346650aee7273b50826d726a4a7a
burhan <burhan.bd@gmail.com>
2024-06-18 13:32:07 +0300

    - added proper docs

--- src/main/java/com/brhn/xpnsr/services/dtos/BillDTO.java
+++ src/main/java/com/brhn/xpnsr/services/dtos/BillDTO.java
@@ -3,7 +3,6 @@ package com.brhn.xpnsr.services.dtos;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import org.springframework.hateoas.RepresentationModel;
 
 import java.io.Serial;
 import java.io.Serializable;
